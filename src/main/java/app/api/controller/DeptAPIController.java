981aa94562b0eaeb973dffd1b3607c4247696d71
HIRedrain <redrain@yu.ac.kr>
2024-12-04 16:18:15 +0900

    DeptAPIController update - appApi();

--- src/main/java/app/api/controller/DeptAPIController.java
+++ src/main/java/app/api/controller/DeptAPIController.java
@@ -27,6 +27,11 @@ public class DeptAPIController {
 	public String deptApi() {
 		return "dept api controller";
 	}
+
+	@GetMapping("/app")
+	public String appApi(){
+		return "Project Changed";
+	}
 	
 	@GetMapping("/depts")
 	public List<Dept> getDepts() {
